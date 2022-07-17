35d51feb1fba5635efdb73074bfa0bbe6a691db1
Matthew Brown <matthew.e.brown.17@gmail.com>
2022-07-17 01:18:30 -0400

    Reset

--- README.md
+++
@@ -1,7 +0,0 @@
-# Personal Site
-
-This is my personal "portfolio" website. Right now it just holds a few links to
-my Stack Overflow, GitHub, LinkedIn, and resume. Eventually it'll be a real
-portfolio.
-
-When that will be, however&hellip; 知らない。
--- scripts/dark-theme.js
+++
@@ -1,9 +0,0 @@
-const blackIcon = document.querySelector('#ico-black');
-const whiteIcon = document.querySelector('#ico-white');
-
-const darkTheme = window.matchMedia('(prefers-color-scheme: dark)');
-darkTheme.onchange = (function onchange() {
-  document.head.removeChild(darkTheme.matches ? blackIcon : whiteIcon);
-  document.head.appendChild(darkTheme.matches ? whiteIcon : blackIcon);
-  return onchange;
-})();
